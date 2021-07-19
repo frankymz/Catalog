9c2d552c90342165b06cde47396da779f8c87587
Francisco <63214442+frankymz@users.noreply.github.com>
2021-07-19 14:17:52 -0400

    structure

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import Service from "../../Service/Service";
+
+function Home() {
+  const [state, setState] = useState({
+    example: []
+  });
+  useEffect(() => {
+    
+  }, []);
+  return (
+    <React.Fragment>
+      <div></div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
---
+++ src/Service/Service.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const rest_api = "http://localhost:8090/api/"
+
+class Service {
+    
+}
+
+export default new Service();
