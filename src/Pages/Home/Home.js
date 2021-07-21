9d9dc219c5c132395be452666e720ec36efeb263
Francisco <63214442+frankymz@users.noreply.github.com>
2021-07-21 12:44:46 -0400

    API is working

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Service from "../../Service/Service";
-import Nav from '../../Components/General-Components/Navigation/Navigation'
 import Panel from "../../Components/Home-Components/Carousel/Coursel";
 
 function Home() {
+  const [state, setState] = useState({
+    products:[]
+  });
+  const[isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    Service.getProducts().then((response)=>{
+      console.log(response.data)
+      setState({products:response.data})
+      setLoading(false);
+    })
+    
+  }, [])
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   
-  return (
+   return (
     <React.Fragment>
+      <div style={{
+        margin:"auto"
+      }}>
+       {JSON.stringify(state.products[0].price)}
+      </div>
+      
       <Panel/>
     </React.Fragment>
   );
--- src/Service/Service.js
+++ src/Service/Service.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const rest_api = "http://localhost:8090/api/"
+const rest_api = "http://localhost:8090"
+
 
 class Service {
-    
+    getProducts(){
+        return axios.get(rest_api +'/products');
+    }
 }
 
 export default new Service();
